f8b2a711385243187fff01ebc20a97801d22a60a
Evan Shortiss <evan.shortiss@feedhenry.com>
2014-12-26 00:38:22 +0000

    Update build

--- dist/fs.js
+++ dist/fs.js
@@ -301,6 +301,7 @@ var utils = _dereq_('./utils')
 var wrapSuccess = utils.wrapSuccess
   , wrapFail = utils.wrapFail;
 
+exports.getFsInstance = fs.getInstance;
 
 exports.appendFile = function(path, data, callback) {
   fs.writeFile(path, data, callback, true);
@@ -398,15 +399,31 @@ exports.mkdir = function(path, callback) {
 };
 
 
+/**
+ * Remove a directory.
+ * The FileSystem API expects directories to be empty but returns a
+ * non-informative error on Android and possibly iOS so we check here
+ * to ensure users know why directory deletes might fail.
+ * @param  {String}   path
+ * @param  {Function} callback
+ */
 exports.rmdir = function(path, callback) {
   var success = wrapSuccess(callback)
     , fail = wrapFail(callback);
 
-  fs.getDirectory(path, function(err, dirEntry) {
+  this.readdir(path, function(err, list) {
     if (err) {
       fail(err);
+    } else if (list && list.length > 0) {
+      fail('ENOTEMPTY: Directory must be empty');
     } else {
-      dirEntry.remove(success, fail);
+      fs.getDirectory(path, function(err, dirEntry) {
+        if (err) {
+          fail(err);
+        } else {
+          dirEntry.remove(success, fail);
+        }
+      });
     }
   });
 };
@@ -420,11 +437,11 @@ exports.exists = function(path, callback) {
     // Don't create the file, just look for it
     create: false
   }, function(err) {
-    if (err && err.code === 1) {
+    if (err && err.code === 1) { // NOT FOUND
       // If the file isn't found we don't want an error, pass false!
       success(false);
     } else if (err) {
-      // An actual error occured
+      // An actual error occured, pass it along
       fail(err);
     } else {
       success(true);
@@ -438,7 +455,7 @@ exports.stat = function(path, callback) {
     , fail = wrapFail(callback)
     , fn = fs.getFile;
 
-  // TODO: Check for folder AND file instead, use whichever exists
+  // TODO: Perhaps check for folder AND file instead, use whichever exists
   if (utils.isDirectory(path)) {
     fn = fs.getDirectory;
   }
